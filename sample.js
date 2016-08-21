294513baca670467faa1ee84342191c1aad9184c
WebTrick <1511134675@qq.com>
2016-08-21 15:31:24 +0800

    Update sample.js

--- sample.js
+++ sample.js
@@ -2,16 +2,16 @@ var app = angular.module('sample',['paginator']);
 
 app.controller('index',function($http,$scope,page){
 
-    //初始化页面数据
+    //initialize the page data
     
-    page.ini(url1,url2);//url1获取总页数大小，url2获取第一页的数据
+    page.ini(url1,url2);//url1 use to get the total pages num，url2 use to get the first page data
     
-    //分页操作,只举例上翻，其余类似
+    //start paginator,other operations similar
     page.up();
-    page.show(url2);//值得注意的，url2是数据源地址，无需加上分页值，page服务会自动加上
+    page.show(url2);//attention,you do not need to add page num to url,as page service will add it auto
     
-    //同一页面多个分页，采取pageChild多服务的形式，当时由于时间原因，没有整合到一个类，不过多服务的扩展只是简单复制也并不复杂，所以也
-    就不再修改
+    //if when you wan to paginator many pages at the same page,then you can use child service,just copy the original,and give it
+    //another name,you can use pageChild to paginator another page then.
     
     
 
